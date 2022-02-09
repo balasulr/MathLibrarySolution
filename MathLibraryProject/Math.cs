737b4dca73330c4b958e89a14ecdcba54f52f788
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-08 21:01:01 -0500

    Add project files.

---
+++ MathLibraryProject/Math.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLibraryProject {
+
+    public static class Math { // Methods in static class have to be static
+       
+        // Polymorphism means that can have more than 1 method with the same name, but
+        // with a different type.
+
+        // Allowed to create many methods with the same name, but have different types.
+        public static string Add(string a, string b) {
+            return a + b;
+        }
+        public static decimal Add(decimal a, decimal b) {
+            return a + b;
+        }
+        public static long Add(long a, long b) {
+            return a + b;
+        }
+        public static int Add(int a, int b) {
+            return a + b;
+        }
+        //////////////////////////////////////////////////////////////////////////
+        public static int Sub(int a, int b) {
+            return a - b;
+        }
+        public static int Mult(int a, int b) {
+            return a * b;
+        }
+        public static int Div(int a, int b) {
+            return a / b;
+        }
+    }
+}
---
+++ MathLibraryProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MathLibraryProject {
+    class Program {
+        static void Main(string[] args) {
+            // Var is implicit and has to return value
+            var a = Math.Add("1", "2"); // Add 2 Strings
+            var b = Math.Add(1, 2); // Add 2 integers
+            var c = Math.Add(1.0m, 2.0m); // Add 2 decimals
+            var d = Math.Add(85477580843434223, 1234567893232343); // Add 2 Long
+            var e = Math.Mult(1, 2);
+            /////////////////////////////////////////////////////////////////////////////////
+
+            // All 4 of the Add methods in Math.cs do the same functionality of adding 2
+            // numbers together, with the only difference being that the 2 numbers being added
+            // together can both be integers, decimal, long or string.
+
+            // For example, there can be Math.Add(1, 2) which adds 2 integers together or
+            // there can be Math.Add("1", "2") which adds 2 strings together.
+
+            // Polymorphism allows each of the methods to work the same & have the same name
+            // at the same time. Instead of having to have 4 separate methods with different
+            // names to add the 4 methods in Math.cs, there can only be one Method name which
+            // takes care of the Add functionality.
+
+            // The question arises of how does the program know which Add method to select.
+            // This is taken care of because the system matches the method to the type of the
+            // numbers entered. For example, if both numbers entered were decimals, the second
+            // Add method would be chosen.
+
+        }
+    }
+}
